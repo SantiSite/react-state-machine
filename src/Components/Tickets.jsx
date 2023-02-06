4bcdd0e9f781373e702e5df47d62db8c37ddd30a
Santiago Restrepo <santiagorv246@gmail.com>
2023-02-05 21:55:11 -0500

    Fix en la carga de imagenes

--- src/Components/Tickets.jsx
+++ src/Components/Tickets.jsx
@@ -9,7 +9,7 @@ export const Tickets = ({ send, context }) => {
     <div className='Tickets'>
       <p className='Tickets-description description'>Gracias por volar con book a fly 💚</p>
       <div className='Tickets-ticket'>
-        <div className='Tickets-country'>Colombia</div>
+        <div className='Tickets-country'>{context.selectedCountry}</div>
         <div className='Tickets-passengers'>
           <span>✈</span>
           {context.passengers.map((passenger, idx) => (
